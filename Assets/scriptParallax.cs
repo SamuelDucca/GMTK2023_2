3459933f8d375badd8c9cd2f185ed2dfd76a81a7
SamuelDucca <samukaducca@hotmail.com>
2023-07-08 15:48:24 -0300

    Added parallax scrolling

--- Assets/scriptParallax.cs
+++ Assets/scriptParallax.cs
@@ -8,23 +8,36 @@ public class scriptParallax : MonoBehaviour
     [SerializeField] bool scrollDown;
 
     float singleTextureHeight;
+    float singleTextureWidth;
+
+    public GameObject wall;
 
     // Start is called before the first frame update
     void Start()
     {
         SetupTexture();
         if(scrollDown) { moveSpeed = -moveSpeed; }
+        wall = GameObject.FindWithTag("RightWall");
     }
     void SetupTexture() 
     { 
         Sprite sprite = GetComponent<SpriteRenderer>().sprite;
         singleTextureHeight = sprite.texture.height / sprite.pixelsPerUnit;
+        singleTextureWidth = sprite.texture.width / sprite.pixelsPerUnit;
     }
 
     void Scroll()
     {
+        wall = GameObject.FindWithTag("RightWall");
         float delta = moveSpeed * Time.deltaTime;
-        transform.position += new Vector3(0f, delta, 0f);
+        //float x_delta = moveSpeed * 0.1f * Time.deltaTime * (transform.position.x - wall.transform.position.x);
+
+        //if ((Mathf.Abs(transform.position.x + x_delta) - singleTextureWidth) <= 0)
+        //{
+        //    x_delta = 0;
+        //}
+
+        transform.position = new Vector3(wall.transform.position.x, transform.position.y + delta, 0f);
     }
 
     void CheckReset() 
